0cac2f6726ba811e2320c598b47d5696691e93e9
Isabella Deng <Isabella.deng2009@gmail.com>
2025-12-05 18:23:48 -0500

    My homework

---
+++ RobotContainer.java
@@ -0,0 +1,101 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.function.IntSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.CoralForceDiffEjectCommand;
+import frc.robot.commands.IntakeCoralCommand;
+import frc.robot.subsystems.AlgaeEndEffectorSubsystem;
+import frc.robot.subsystems.CoralEndEffectorSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  // subsystems
+  private final CoralEndEffectorSubsystem coralEndEffectorSubsystem = new CoralEndEffectorSubsystem();
+  private final AlgaeEndEffectorSubsystem algaeEndEffectorSubsystem = new AlgaeEndEffectorSubsystem();
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+    // this is a suggested answer! your answer does not need to look exactly like this
+    // just make sure it performs the same function
+    SmartDashboard.putString("Section:", "Coral End Effector Subsystem");
+    SmartDashboard.putBoolean("Is Lower Break Beam On:", coralEndEffectorSubsystem.isLowerBeamBroken());
+    SmartDashboard.putBoolean("Is Upper Break Beam On:", coralEndEffectorSubsystem.isUpperBeamBroken());
+    SmartDashboard.putNumber("Algae Encoder Value:", algaeEndEffectorSubsystem.getEncoderValue());
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+
+  private final IntSupplier lvl = () -> 1;
+
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    m_driverController.leftBumper()
+        .onTrue(Commands.run(() -> 
+        coralEndEffectorSubsystem.setVolt(7), coralEndEffectorSubsystem)
+            .until(() -> 
+          !coralEndEffectorSubsystem.isLowerBeamBroken())
+            .andThen(() -> 
+          coralEndEffectorSubsystem.stop()));
+
+    m_driverController.rightBumper().onTrue(new IntakeCoralCommand(coralEndEffectorSubsystem));
+    m_driverController.a().onTrue(new CoralForceDiffEjectCommand(coralEndEffectorSubsystem, lvl));
+    // left = eject
+    // a = force diff eject
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
+    // cancelling on release.
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return null;
+  }
+}
